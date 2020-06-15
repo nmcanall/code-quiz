bbc4eff05901d4e6c2376bd0890ecc40fa1ab0b6
nmcanall <nmcanall@citadel.edu>
2020-06-14 18:09:18 -0700

    All quiz questions appear correctly

--- assets/js/quiz-script.js
+++ assets/js/quiz-script.js
@@ -7,22 +7,22 @@ var quizQuestions = [
     {
         q: "What color is the sky?",
         answers: ["Orange", "Blue", "Green", "Yellow"],
-        correctAnswer: 1 //Index of answer
+        correctAnswer: "Blue"
     },
     {
         q: "What's your dog's name?",
         answers: ["Blue", "Hub", "Bud", "Peter"],
-        answer: 4
+        answer: "Peter"
     },
     {
         q: "Who plays Forrest Gump?",
         answers: ["Tom Hanks", "Denzel Washington", "Johnny Depp", "Leonardo DiCaprio"],
-        answer: 1
+        answer: "Tom Hanks"
     },
     {
         q: "What's the answer to life?",
         answers: ["Happiness", "Money", "42", "Power"],
-        answer: 3
+        answer: "42"
     }
 ]
 var timeRemaining = quizQuestions.length * 15; // Allows 15 seconds per question
@@ -45,7 +45,23 @@ var startTimer = function() {
 
 // Function to process when a quiz answer is clicked
 var submitAnswer = function(event) {
+    var targetEl = event.target;
+    
+    // Ensure user clicked on an answer option; if not, return
+    if(targetEl.nodeName !== "LI") {
+        return;
+    }
 
+    // Store users answer and change score
+    var userAnswer = targetEl.textContent;
+
+    // If not at the end of the quiz, display the next question; otherwise, end the quiz
+    if(currentQuestion < quizQuestions.length) {
+        newQuizQuestion();
+    }
+    else {
+        endQuiz();
+    }
 }
 
 // Function to build a new question
@@ -53,6 +69,11 @@ var newQuizQuestion = function() {
 
     // This is the list for possible answers
     var answerListEl = document.querySelector("#answer-list");
+
+    // Clear any current childer of answerListEl
+    while(answerListEl.firstChild) {
+        answerListEl.removeChild(answerListEl.childNodes[0]);
+    }
 
     // Display new quiz question
     document.querySelector("#quiz-question").textContent = quizQuestions[currentQuestion].q;
@@ -64,6 +85,9 @@ var newQuizQuestion = function() {
         answerEl.textContent = quizQuestions[currentQuestion].answers[i];
         answerListEl.appendChild(answerEl);
     }
+
+    // Move on to the next question
+    currentQuestion++;
 }
 
 // Helper function to end the quiz
